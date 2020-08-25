8a14c9e0507fecfce5a8e7257a26efe9957c4b49
Alina Lizyaeva <l_alin@mail.ru>
2020-08-25 15:09:55 +0600

    Added PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,13 @@
+package ru.netology.manager;
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerId, String domain, String query, int ownerOnly, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int postId, int ownerId) {
+    }
+}
